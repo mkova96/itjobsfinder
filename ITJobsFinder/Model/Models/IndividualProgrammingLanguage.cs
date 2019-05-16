5049b3864f9612698ccd87776b0441899f2c8503
mkova <mario.kovacevic@fer.hr>
2019-05-16 17:18:31 +0200

    dodani modeli
    Signed-off-by: mkova <mario.kovacevic@fer.hr>

---
+++ ITJobsFinder/Model/Models/IndividualProgrammingLanguage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITJobsFinder.Model.Models
+{
+    public class IndividualProgrammingLanguage
+    {
+        public Individual Individual { get; set; }
+        public ProgrammingLanguage ProgrammingLanguage { get; set; }
+
+    }
+}
