3ac122348b5e872445fc3f19a3ab6c93a189df86
mkova <mario.kovacevic@fer.hr>
2019-05-16 21:38:38 +0200

    dodani modeli i dodana migracija
    Signed-off-by: mkova <mario.kovacevic@fer.hr>

---
+++ ITJobs/ITJobs.DLL/ITJobsContext.cs
@@ -0,0 +1,24 @@
+﻿using ITJobs.Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITJobs.DLL
+{
+    public class ITJobsContext : DbContext
+        {
+            public ITJobsContext(DbContextOptions<ITJobsContext> options)
+                : base(options)
+            { }
+
+            public DbSet<ProgrammingLanguage> ProgrammingLanguage { get; set; }
+            public DbSet<Ad> Ad { get; set; }
+            public DbSet<AdCategory> AdCategory { get; set; }
+            public DbSet<Company> Company { get; set; }
+            public DbSet<DataBase> DataBase { get; set; }
+            public DbSet<Individual> Individual { get; set; }
+            public DbSet<IndividualDataBase> IndividualDataBase { get; set; }
+            public DbSet<IndividualProgrammingLanguage> InduvidualProgrammingLanguage { get; set; }
+        }
+}
---
+++ ITJobs/ITJobs.Model/Models/IndividualDataBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITJobs.Model.Models
+{
+    public class IndividualDataBase
+    {
+        public int Id { get; set; }
+        public Individual Individual { get; set; }
+        public DataBase DataBase { get; set; }
+    }
+}
